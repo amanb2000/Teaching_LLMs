c7f2c890cb8af0f4f188ec3ce67729f06db13bfb
Aman Bhargava <amanb2000@gmail.com>
2023-12-11 00:22:40 -0800

    Initial commit: activation visualization for LLM

---
+++ sketch/key_value_bitmaps.py
@@ -0,0 +1,15 @@
+# %%
+"""
+# Key-Value Bitmaps
+
+This script plots the key and value representaitons for a given input sentence 
+to an LLM. 
+
+This should work for any HuggingFace LLM. 
+"""
+
+# %%
+
+
+
+# %%
